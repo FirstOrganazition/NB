a46a79d55be57558219d71db6c7a3d3ddcaee688
llj <llj@qq.com>
2016-05-16 16:50:59 +0800

    new
    new

---
+++ bkb/bkb/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  bkb
+//
+//  Created by tarena51 on 16/5/16.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
